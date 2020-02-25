1f4f3a764dc2f19fb4e47ea1d873cb3dfc979c8c
FloPereira75 <44778588+FloPereira75@users.noreply.github.com>
2020-02-25 04:26:33 +0100

    new mp

--- Projets/Prisma Media/main.js
+++ Projets/Prisma Media/main.js
@@ -1,3 +1,28 @@
+function parallax() {
+    const parallax = document.querySelector('.header');
+    const parallaxKiloRen = document.querySelector('.bckg-img-btw-article');
+    const parallaxStormTrooper = document.querySelector('.image-after-article');
+
+    window.addEventListener('scroll', () => {
+        parallax.style.backgroundPositionY = window.scrollY / 3 + "px";
+    });
+
+    window.addEventListener('scroll', () => {
+        parallaxKiloRen.style.backgroundPositionY = window.scrollY / 3 + "px";
+    });
+
+    window.addEventListener('scroll', () => {
+        parallaxStormTrooper.style.backgroundPositionX = window.scrollY / 15 + "px";
+    });
+};
+
+parallax();
+
+
+
+
+
+
 $(document).ready(function() {
     /* Ouvrir le menu */
     $('.menu-btn').click(function() {
